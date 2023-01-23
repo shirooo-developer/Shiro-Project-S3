0b6908c439b64b303a380f6245b8f813d557aa8d
Shirooo Official <107530906+shirooo-developer@users.noreply.github.com>
2023-01-23 20:55:56 +0800

    Add files via upload

--- plugins/main-infoupdate.js
+++ plugins/main-infoupdate.js
@@ -1,142 +1,14 @@
 let handler = async m => m.reply(`
-*PATCH NOTE UP V.45*
+*LAST UPDATE 🆙*
 
-*Fitur Baru 🔥*
-*_#tiktokstalk_*
-*_#listadmin_*
-*_#listblock_*
-*_#playstore_*
-*_#pack_*
-*_#pack2_*
-*_#pack3_*
-*_#videoxxx_*
-*_#videoxxxlesbi_*
-*_#stickerpack_*
-*_#kicknum_*
-*_#listanum_*
-*_#repeat_*
-*_#hornycard_*
-*_#iss_*
-*_#logo_*
-*_#pixel_*
-*_#ytcomment_*
-*_#lolice_*
-*_#simpcard_*
-*_#jadianime_*
-*_#itzy_*
-*_#cat_*
-*_#natal_*
-*_#animerandom_*
-*_#alkitab_*
-*_#animal_*
-*_#gunung_*
-*_#hacker_*
-*_#kanji_*
-*_#toloserti_*
-*_#fuckboy_*
-*_#fuckgirl_*
-*_#bucinserti_*
-*_#goodboy_*
-*_#goodgirl_*
-*_#badboy_*
-*_#badgirl_*
-*_#anime_*
-*_#charagenshin_*
+*_Fitur Terakhir Kali Yang Diupdate_*
+*_Date: 23/01/2023_*
 
-*Fitur Difix 🛠️*
-*_#tiktok_*
-*_#wikipedia_*
-*_#simi_*
-*_#akira_*
-*_#akiyama_*
-*_#anna_*
-*_#asuna_*
-*_#ayuzawa_*
-*_#boruto_*
-*_#chiho_*
-*_#chitoge_*
-*_#deidara_*
-*_#erza_*
-*_#elaina_*
-*_#eba_*
-*_#emilia_*
-*_#hestia_*
-*_#hinata_*
-*_#inori_*
-*_#isuzu_*
-*_#itachi_*
-*_#itori_*
-*_#kaga_*
-*_#kagura_*
-*_#kaori_*
-*_#keneki_*
-*_#kotori_*
-*_#kurumi_*
-*_#madara_*
-*_#mikasa_*
-*_#miku_*
-*_#minato_*
-*_#naruto_*
-*_#nezuko_*
-*_#sagiri_*
-*_#sasuke_*
-*_#sakura_*
-*_#cosplay_*
-*_#fanart_*
-*_#husbu_*
-*_#kanna_*
-*_#megumin_*
-*_#loli_*
-*_#neko_*
-*_#otaku-desu_*
-*_#waifu_*
-*_#tebaklagu_*
-*_#exo_*
-*_#bts_*
-*_#cecan_*
+#menu
+#title-list
+#infoupdate
 
-*Fitur Dihapus 🚫*
-*_#igstalk_*
-*_#mediafire_*
-*_#gimage_*
-*_#kusonime_*
-*_#rize_*
-*_#shina_*
-*_#shota_*
-*_#cerpen_*
-*_#tiktokmp3_*
-*_#xnxxsearch_*
-*_#xnxxdl_*
-*_#asahotak_*
-*_#tebakgombal_*
-*_#suitpvp_*
-*_#sambungkata_*
-*_#susunkata_*
-*_#tebakchara_*
-*_#tebakjenaka_*
-*_#tebakkalimat_*
-*_#tebakkabupaten_*
-*_#tebakkimia_*
-*_#tebaklirik_*
-*_#tekateki_*
-*_#hd_*
-*_#indohot_*
-*_#covid_*
-*_#messi_*
-*_#ronaldo_*
-*_#hadis_*
-*_#logololi_*
-*_#logokaneki_*
-*_#logorem_*
-*_#logoneko_*
-*_#logosadboy_*
-*_#agedetect_*
-*_#ephoto_*
-*_#lisapresent_*
-*_#gta_*
-*_#wallq_*
-
-*Perubahan Fitur 🔄*
+*THANK YOU!!!*
 `.trim()) // Tambah sendiri kalo mau
 
 
