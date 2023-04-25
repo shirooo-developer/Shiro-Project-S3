fdb2ef0bbfa309ad036931eaffd8c63e8756cb40
Shirooo Official <107530906+shirooo-developer@users.noreply.github.com>
2023-04-25 16:39:30 +0800

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,13 +1,15 @@
 let handler = async m => m.reply(`
 Mau Lihat Menu? Lihat Di *.? Menu*
-
 Perhatiakan Tanda *.?*
+
+Jika Menu Tidak Terlihat
+Daftar Dulu *#daftar*
 `.trim()) // Tambah sendiri kalo mau
 
 
 handler.help = ['menu']
 handler.tags = ['main','rpg']
 handler.command = /^menu$/i
-handler.register = true
+handler.register = false
 handler.premium = false
 export default handler
